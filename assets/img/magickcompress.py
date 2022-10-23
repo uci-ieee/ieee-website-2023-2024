7d390a42d4383185bd02b9f0217297638bf4f564
leavenember <64881497+leavenember@users.noreply.github.com>
2022-10-23 13:42:31 -0700

    img compress, MM Officer fix, new events

--- assets/img/magickcompress.py
+++ assets/img/magickcompress.py
@@ -1,19 +1,27 @@
+# compresses images in dirlist
+# run in ieee-website-2022-2023/assets/img directory
+
 import sys, os, subprocess
 from pathlib import Path
 #dirlist = os.listdir(sys.argv[1])
-compressdir = "dirhere/"
+#compressdir = "events/" # directory of images to be compressed
+compressdir = "micromouse/"
 dirlist = os.listdir(compressdir)
 print(dirlist)
 for file in dirlist:
     
-    if file.endswith("jpg"):
+    if (file.endswith("jpg") | file.endswith("png")):
         filepath = Path('./'+compressdir+file)
-        print(filepath)
-        cmd = ['magick', str(filepath), '-quality', str(99), 
-            '-define', 'webp:near-lossless=99', 
-            '-define', 'webp:method=6',
-            '-define', 'webp:target-size=100000', 
-            '-define', 'webp:thread-level=1', 
-            str(filepath.with_suffix('.webp'))]
-        subprocess.call(cmd)
+        print(filepath, end =" ")
+        if not os.path.exists(str(filepath.with_suffix('.webp'))): # doesn't rerun on previously converted images
+            print("")
+            cmd = ['magick', str(filepath), '-quality', str(99), 
+                '-define', 'webp:near-lossless=99', 
+                '-define', 'webp:method=6',
+                '-define', 'webp:target-size=100000', 
+                '-define', 'webp:thread-level=1', 
+                str(filepath.with_suffix('.webp'))]
+            subprocess.call(cmd)
+        else:
+            print("--SKIPPED")
         #input("stop")
